9412f754c280afe1c8eb0735f672ecc3a93b9a88
Feft <piotrpikula@gmail.com>
2017-09-16 13:49:11 +0200

    doctrine with transaction, myisam and rabbitmq producer with durable option

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Data;
+use AppBundle\Entity\DataMyisam;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -34,6 +35,52 @@ class DefaultController extends Controller
         $em->persist($data);
         $em->flush();
 
+        return new JsonResponse();
+    }
+    /**
+     * Simple doctrine use, no sql transaction, MyISAM table engine
+     *
+     * @Route("/doctrineMyisam", name="doctrineMyisam")
+     */
+    public function doctrineMyisamAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $data = new DataMyisam();
+        $data->setDescription($this->getData());
+        $em->persist($data);
+        $em->flush();
+
+        return new JsonResponse();
+    }
+
+    /**
+     * Simple doctrine use with sql transaction
+     *
+     * @Route("/doctrineTransaction", name="doctrineTransaction")
+     */
+    public function doctrineTransactionAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $data = new Data();
+        $data->setDescription($this->getData());
+
+        $em->getConnection()->beginTransaction();
+        $em->persist($data);
+        $em->flush();
+        $em->getConnection()->commit();
+
+        return new JsonResponse();
+    }
+
+    /**
+     * Simple rabbitmq message publish
+     *
+     * @Route("/rabbitmq", name="rabbitmq")
+     */
+    public function rabbitmqAction()
+    {
+        $this->get("old_sound_rabbit_mq.api_call_producer")->publish($this->getData());
+//        $this->get("old_sound_rabbit_mq.api_call_transient_producer")->publish($this->getData());
         return new JsonResponse();
     }
 
